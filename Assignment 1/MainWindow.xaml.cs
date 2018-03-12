79ccc8a6ec4cef9259c7212f5237c2b51e4198a2
Samuel.a.m.f <Samuel.a.m.f@gmail.com>
2018-03-12 00:42:28 -0700

    Added Assignment 1

---
+++ Assignment 1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HelloWorld.Models.User user = new HelloWorld.Models.User();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public override void EndInit()
+        {
+            base.EndInit();
+            uxContainer.DataContext = user;
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password: " + uxPassword.Text + "\n" + "And it works!");
+        }
+    }
+}
